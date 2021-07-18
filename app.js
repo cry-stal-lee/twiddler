251a67c08491f95ee93738c76b70fb7ac5ec39ad
Crystal Lee <crystalleedev@gmail.com>
2021-07-18 09:54:43 -0400

    Completed Libraries and APIs

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
   // Select already existing elements
 
   // Create new HTML elements
@@ -25,11 +24,11 @@ $(document).ready(function(){
     var $profilePhoto = $('<img class="profile-photo"></img>').attr('src', tweet.profilePhotoURL);
     var $user = $('<span class="username"></span>').text('@' + tweet.user);
     var $message = $('<p class="message"></p>').text(tweet.message);
-    var $timestamp = $('<span class="timestamp"></span>').text(tweet.created_at);
-    var $comment = $('<img class="icon comment"></img>').attr('src', './assets/icons/placeholder.png');
-    var $retweet = $('<img class="icon retweet"></img>').attr('src', './assets/icons/placeholder.png');
-    var $like = $('<img class="icon like"></img>').attr('src', './assets/icons/placeholder.png');
-    var $share = $('<img class="icon share"></img>').attr('src', './assets/icons/placeholder.png');
+    var $timestamp = $('<span class="timestamp"></span>').text(jQuery.timeago(tweet.created_at));
+    var $comment = $('<i class="fas fa-comment icon comment"></i>');
+    var $retweet = $('<i class="fas fa-retweet icon retweet"></i>');
+    var $like = $('<i class ="fas fa-heart icon like"></i>');
+    var $share = $('<i class="fas fa-share icon share"></i>');
     $tweet.append($profilePhoto, '<br>', $user, $message, $timestamp,
         '<br>', $comment, $retweet, $like, $share);
     return $tweet;
@@ -39,6 +38,13 @@ $(document).ready(function(){
   $updateFeed.on("click", function(event) {
     renderFeed(event);
   });
+
+  // $('.icon').hover(
+  //   function() {
+  //     $(this.element).addClass("hover");
+  //   }, function() {
+  //     $(this.element).removeClass("hover");
+  // });
 
   // Append new HTML elements to the DOM
   $title.appendTo($app);
