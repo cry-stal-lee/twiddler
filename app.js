20bf1f647dc41aff0210045a2826c594927fef1f
Crystal Lee <crystalleedev@gmail.com>
2021-07-20 08:47:15 -0400

    Added fade effect to slidetoggle for friends list

--- app.js
+++ app.js
@@ -54,8 +54,14 @@ $(document).ready(function(){
     refreshNewTweeds();
     if (user) {
       var streamSource = streams.users[user];
+      $newTweedFormContainer.fadeOut();
+      $tweedItContainer.fadeOut();
+      $newTweeds.fadeOut();
     } else {
       var streamSource = streams.home;
+      $newTweedFormContainer.fadeIn();
+      $tweedItContainer.fadeIn();
+      $newTweeds.fadeIn();
     }
     var index = streamSource.length - 1;
     while(index >= 0){
@@ -193,7 +199,10 @@ $(document).ready(function(){
 
   $('.menu-button').on("click", function(x) {
     $(this).toggleClass("change");
-    $('.menu').slideToggle("slow");
+    $(".menu").animate({
+      height: "toggle",
+      opacity: "toggle"
+    }, "slow");
   });
 
   // Autoexpand and shrink text area
@@ -219,6 +228,7 @@ CSS background gradient animator: https://www.gradient-animator.com/
 Auto-resize textarea: https://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize / http://jsfiddle.net/n9uuv0hd/54/
 Transforming hamburger menu icon: https://www.w3schools.com/howto/howto_css_menu_icon.asp
 Fill color left to right in CSS: https://stackoverflow.com/questions/17212094/fill-background-color-left-to-right-css
+JQuery slidetoggle + fade effect: https://stackoverflow.com/questions/7672556/how-to-add-an-opacity-fading-effect-to-to-the-jquery-slidetoggle
 
 */
 
